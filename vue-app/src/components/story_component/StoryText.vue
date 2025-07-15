11b8783cf15819bedc41d24766400810aeb1e628
adtpstudent <atdpstudent@no-email.ocf.berkeley.edu>
2025-07-15 14:19:12 -0700

    PLAN: Created files for Component Strcuture

---
+++ vue-app/src/components/story_component/StoryText.vue
@@ -0,0 +1,12 @@
+<script>
+
+// Gonna plan out Story and Lore here
+// This is going to be the logic that will send up teh story etxt using json
+// Retrieve them from story.json
+
+</script>
+
+
+
+
+<template></template>
