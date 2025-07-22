95258821347c7c4302f9a5e7d4aab55e1ef331f5
aGuyEatingRamen <namanjainreal@gmail.com>
2025-07-21 20:45:25 -0700

    PLAN: Better commenting and new features. Updated story a whole lot to create an okay plot for now. Sorry fro my sloppy typing in last commintent.

--- vue-app/src/components/Story.vue
+++ vue-app/src/components/Story.vue
@@ -7,7 +7,6 @@ import StoryText from "./story_component/StoryText.vue"
 import Decesion from "./story_component/DecesionButton.vue"
 import storyJson from "../assets/data/story.json"
 
-
 export default {
   components: {
     Decesion,
@@ -16,36 +15,42 @@ export default {
   data() {
     return {
       chapter: 0,
-      event: 0
+      event: 0,
+      showButtons: false  
+      // Controls button visibility
     }
   },
   methods: {
-    neEvent( redirect ){
-      console.log(redirect);
-      for(let i = 0; i < storyJson[this.chapter].events.length; i++){
-        if((storyJson[this.chapter].events[i].title) == (redirect)){
-          console.log("match",i)
+    neEvent(redirect) {
+      this.showButtons = false; 
+       // Hide buttons when new event loads (new text starts)
+      for (let i = 0; i < storyJson[this.chapter].events.length; i++) {
+        if (storyJson[this.chapter].events[i].title === redirect) {
           this.event = i;
+          break;
         }
       }
+    },
+    onTypingDone() {
+      this.showButtons = true;  
+      // Show buttons only when typing finishes
     }
-
-  },
-  computed: {
-  },
-  mounted() {
   }
 }
-
-
-
 </script>
 
-
-
-
 <template>
-    <StoryText :chapter="chapter" :event="event"/>
-    <Decesion :chapter="chapter" :event="event" @newEvent = "(redirect)=> neEvent(redirect)" />
+  <StoryText 
+    :chapter="chapter" 
+    :event="event" 
+    @typingDone="onTypingDone" 
+  />
+  <Decesion 
+    v-if="showButtons" 
+    :chapter="chapter" 
+    :event="event" 
+    @newEvent="neEvent" 
+  />
 </template>
+
 
--- vue-app/src/components/story_component/StoryText.vue
+++ vue-app/src/components/story_component/StoryText.vue
@@ -4,22 +4,62 @@
 // This is going to be the logic that will send up teh story etxt using json
 // Retrieve them from story.json and then shoot them up the DOM
 import storyJson from "../../assets/data/story.json"
-
 export default {
   props: ['chapter', 'event'],
-  computed: {
-    storyText() {
-      return storyJson[this.chapter]?.events?.[this.event]?.text || "Story text not found.";
+  data() {
+    return {
+      fullText: "",
+      displayedText: "",
+      index: 0,
+      interval: null
+    };
+  },
+  watch: {
+    // Watch for chapter or event change to restart typing
+    chapter: "startTyping",
+    event: "startTyping"
+  },
+  methods: {
+    startTyping() {
+      this.resetTyping();
+      this.fullText = storyJson[this.chapter]?.events?.[this.event]?.text || "Text not found.";
+      this.index = 0;
+
+      this.interval = setInterval(() => {
+        if (this.index < this.fullText.length) {
+          this.displayedText += this.fullText[this.index];
+          this.index++;
+        } else {
+          clearInterval(this.interval);
+        }
+      }, 30); 
+      // Typing speed here
+
+
+      if (this.index >= this.fullText.length) {
+        clearInterval(this.interval);
+        this.$emit('typingDone');
+      } 
+      // Sends the siginal for decesion buttons to appear
+    },
+    resetTyping() {
+      clearInterval(this.interval);
+      //  Typing get reset through inteverval
+      this.displayedText = "";
+      this.index = 0;
     }
   },
   mounted() {
-    console.log("StoryText mounted. Current event:", this.event);
+    this.startTyping();
+  },
+  beforeDestroy() {
+    clearInterval(this.interval);
   }
-}
+};
 </script>
 
 <template>
-  <div class="story" id="story">{{ storyText }}</div>
+  <div class="story" id="story">{{ displayedText }}</div>
 </template>
 
 <style>
@@ -32,5 +72,7 @@ export default {
   margin: 10rem auto 20rem auto;
   color: #fff;
   text-shadow: 0 0 10px red, 0 0 20px red;
+  min-height: 6rem;
+  white-space: pre-wrap;
 }
 </style>
